7c81691d7d8127aa5fda8b869000e7cdaedfbd75
A <99933422+data-sandbox@users.noreply.github.com>
2023-08-14 18:53:58 -0600

    Init website
    TODO: add additional sections

---
+++ README.md
@@ -0,0 +1,3 @@
+# data-sandbox.github.io
+
+Personal website built using HTML, CSS, and JavaScript.
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import loadProjects from "./projects";
+// import './style.css';
+
+loadProjects();
+
+// Nav hamburger selections
+const burger = document.querySelector("#hamburger");
+const ul = document.querySelector("nav ul");
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+  closeIcon.classList.toggle("show");
+  menuIcon.classList.toggle("hide");
+});
+
+// Close hamburger menu when a link is clicked or click outside menu
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+    closeIcon.classList.remove("show");
+    menuIcon.classList.remove("hide");
+  })
+);
+
+function handleOutsideClick(e) {
+  if (ul.classList.contains('show') && e.target.id != 'menu-icon') {
+    ul.classList.remove("show");
+    closeIcon.classList.remove("show");
+    menuIcon.classList.remove("hide");
+  }
+}
+
+document.addEventListener('click', handleOutsideClick)
